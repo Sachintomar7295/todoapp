36fb19290990a8a56330aadb6ab18c57bc3e5a77
Your Name <youremail@example.com>
2025-05-27 17:45:05 +0530

    root change

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
         <TodoProvider>
           <Navbar />
           <Routes>
+            <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/signup" element={<Signup />} />
             <Route
